ff11ee85c270dd9f7ec56806a0f859e1bc19c9e1
Kristofer Kirss <kristofer.kirss@hotmail.com>
2018-06-07 22:41:22 +0300

    Fix silly bugs and readd shops command

--- kriisis_page/telegram_bot/kriisis_bot.py
+++ kriisis_page/telegram_bot/kriisis_bot.py
@@ -38,7 +38,7 @@ class KriisisBot(telegram.Bot):
         dispatcher.add_handler(CommandHandler("removeshop", self.__class__.removeshop_command, pass_args=True))
         dispatcher.add_handler(CommandHandler("removecategory", self.__class__.removecategory_command, pass_args=True))
         dispatcher.add_handler(CommandHandler("github", self.__class__.github_command))
-        # dispatcher.add_handler(CommandHandler("shops", self.__class__.shops_command, pass_args=True))
+        dispatcher.add_handler(CommandHandler("shops", self.__class__.shops_command))
         # dispatcher.add_handler(CommandHandler("find", self.__class__.find_command, pass_args=True))
 
     def start_bot(self):
@@ -83,12 +83,12 @@ class KriisisBot(telegram.Bot):
     def notify_user(self, profile, discount):  # TODO: Implement notifying the user
         if profile.telegram_picture_notifications:
             if discount.image_file_id is not None:
-                self.send_photo(profile.chat_id, discount.image_file_id)
+                self.send_photo(profile.telegram_chat_id, discount.image_file_id)
             else:
-                message = self.send_photo(profile.chat_id, discount.image_url)
+                message = self.send_photo(profile.telegram_chat_id, discount.image_url)
                 discount.image_file_id = message.photo[0].file_id
                 discount.save()
-        self.send_message(profile.chat_id, discount.notification_str, parse_mode=ParseMode.MARKDOWN)
+        self.send_message(profile.telegram_chat_id, discount.notification_str, parse_mode=ParseMode.MARKDOWN)
 
     def scrape(self, job):
         new_discounts = self.scraper.scrape_discounts()
@@ -102,10 +102,10 @@ class KriisisBot(telegram.Bot):
 
     def get_profile(self, update):
         try:
-            profile = Profile.objects.get(telegran_user_id=update.message.from_user.id)
+            profile = Profile.objects.get(telegram_user_id=update.message.from_user.id)
         except ObjectDoesNotExist:
             self.send_message(update.message.chat_id, "Please use /start first.")
-            raise RuntimeError("User hadn't used /start")
+            self.logger.debug("User hadn't used /start")
         else:
             return profile
 
@@ -157,7 +157,7 @@ class KriisisBot(telegram.Bot):
             if type_ is Category or type_ is None:
                 found_objects.extend(list(Category.objects.filter(name__icontains=search_query).all()))
             if not found_objects:
-                self.send_message(profile.chat_id, "Found nothing with that name.")
+                self.send_message(profile.telegram_chat_id, "Found nothing with that name.")
                 return
         else:
             if type_ is Shop or type_ is None:
@@ -165,20 +165,20 @@ class KriisisBot(telegram.Bot):
             if type_ is Category or type_ is None:
                 found_objects.extend(list(Category.objects.filter(kriisis_id=obj_id).all()))
             if not found_objects:
-                self.send_message(profile.chat_id, "Found nothing with that id.")
+                self.send_message(profile.telegram_chat_id, "Found nothing with that id.")
                 return
         applicable_objects = []
         for found_object in found_objects:
             if type(found_object) is Shop:
                 if remove:
-                    applicable = found_object in profile.subscribed_shops
+                    applicable = found_object in profile.subscribed_shops.all()
                 else:
-                    applicable = found_object not in profile.subscribed_shops
+                    applicable = found_object not in profile.subscribed_shops.all()
             elif type(found_object) is Category:
                 if remove:
-                    applicable = found_object in profile.subscribed_categories
+                    applicable = found_object in profile.subscribed_categories.all()
                 else:
-                    applicable = all(category not in profile.subscribed_categories for category in
+                    applicable = all(category not in profile.subscribed_categories.all() for category in
                                      [found_object] + list(found_object.ancestors))
             else:
                 raise NotImplementedError("Unknown object found in add_command")
@@ -187,14 +187,14 @@ class KriisisBot(telegram.Bot):
         if len(applicable_objects) == 0:
             if remove:
                 if len(found_objects) == 1:
-                    self.send_message(profile.chat_id, "You aren't subscribed to it.")
+                    self.send_message(profile.telegram_chat_id, "You aren't subscribed to it.")
                 else:
-                    self.send_message(profile.chat_id, "You aren't subscribed to them.")
+                    self.send_message(profile.telegram_chat_id, "You aren't subscribed to them.")
             else:
                 if len(found_objects) == 1:
-                    self.send_message(profile.chat_id, "You already are subscribed to it.")
+                    self.send_message(profile.telegram_chat_id, "You already are subscribed to it.")
                 else:
-                    self.send_message(profile.chat_id, "You already are subscribed to them.")
+                    self.send_message(profile.telegram_chat_id, "You already are subscribed to them.")
         elif len(applicable_objects) == 1:
             applicable_object = applicable_objects[0]
             if type(applicable_object) is Shop:
@@ -211,14 +211,14 @@ class KriisisBot(telegram.Bot):
                 raise NotImplementedError("Unknown object found in add_command")
             profile.save()
             if remove:
-                self.send_message(profile.chat_id, "You removed {}.".format(str(applicable_object)))
+                self.send_message(profile.telegram_chat_id, "You removed {}.".format(str(applicable_object)))
             else:
-                self.send_message(profile.chat_id, "You subscribed to {}.".format(str(applicable_object)))
+                self.send_message(profile.telegram_chat_id, "You subscribed to {}.".format(str(applicable_object)))
         else:
             message = "Multiple matches found, please be more specific or use the id:"
             for applicable_object in applicable_objects:
                 message += "\n" + str(applicable_object)
-            self.send_message(profile.chat_id, message)
+            self.send_message(profile.telegram_chat_id, message)
 
     def remove_command(self, update, args, type_=None):
         self.add_command(update, args, type_=type_, remove=True)
@@ -239,16 +239,15 @@ class KriisisBot(telegram.Bot):
         chat_id = update.message.chat_id
         self.send_message(chat_id, self.GITHUB_LINK)
 
-        # def shops_command(self, update):
-        #     chat_id, user_id = update.message.chat_id, update.message.from_user.id
-        #     user = self.database.get_profile(user_id)
-        #     message = "All shops:"
-        #     for shop in Shop2.get_all_shops():
-        #         message += "\n" + str(shop.shop_id) + ": " + shop.name
-        #         if user.has_shop():
-        #             message += " (added)"
-        #     self.send_message(chat_id, message)
-        #
+    def shops_command(self, update):
+        profile = self.get_profile(update)
+        message = "All shops:"
+        for shop in Shop.objects.order_by("kriisis_id").all():
+            message += "\n{shop.kriisis_id}: {shop.name}".format(shop=shop)
+            if shop in profile.subscribed_shops.all():
+                message += " (added)"
+        self.send_message(profile.telegram_chat_id, message)
+
         # def find_command(self, update, args):
         #     max_categories = 20
         #     chat_id, user_id = update.message.chat_id, update.message.from_user.id
